cce97d0f5f53fd6e57a7649f580c1e8c235c9210
ohyuna56na <safinatunnajahnajwa@gmail.com>
2025-03-11 20:08:55 +0700

    Update db.js

--- db.js
+++ db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2');
 
 // Setup koneksi database
 const pool = mysql.createPool({
-  host: 'localhost',  // Ganti dengan host DB yang sesuai
+  host: 'hopper.proxy.rlwy.net',  // Ganti dengan host DB yang sesuai
   user: 'root',       // Ganti dengan user DB yang sesuai
   password: '',       // Ganti dengan password DB yang sesuai
   database: 'kulinerkitav2', // Ganti dengan nama database yang sesuai
+  port: 35797
 });
 
 module.exports = pool.promise();  // Menyediakan koneksi pool dengan promise
