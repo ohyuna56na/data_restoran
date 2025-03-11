651b5a68de0aba75d13c1d47532ac151c06d0b1d
ohyuna56na <safinatunnajahnajwa@gmail.com>
2025-03-11 16:44:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const db = require('./db');
 const app = express();
-const port = 3000;
+const port = 3306;
 
 // Helper function untuk query database
 const queryDatabase = async (query, params = []) => {
